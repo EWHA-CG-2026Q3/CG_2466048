bf57323da60068c5f86510e550f9a8090d62e7b9
CinnaPie <73657005+CinnaPie@users.noreply.github.com>
2026-09-13 23:31:48 +0900

    feat: 과제1번

---
+++ Assets/S03/S03_test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class S03_test : MonoBehaviour
+{
+    [SerializeField] private Vector3 targetPosition;
+    [SerializeField] private Vector3 targetRotationEuler;
+    [SerializeField] private Vector3 targetScale = Vector3.one;
+
+    void Update()
+    {
+        transform.position = targetPosition;
+        transform.rotation = Quaternion.Euler(targetRotationEuler);
+        transform.localScale = targetScale;
+    }
+}
