445def293eda5972c8aa3a3241c289884b5b0eae
Velde <cloudtail143@ewha.ac.kr>
2026-09-20 21:31:42 +0900

    feat: 칠각형 만들기

--- Assets/S03/S03_CustomPolygonMesh_Square.cs
+++ Assets/S03/S03_CustomPolygonMesh_Square.cs
@@ -8,10 +8,12 @@
         Vector3[] vertices = new Vector3[]
         {
             new Vector3( 0.00f,  3.00f, 0f), // 0 위
-            new Vector3( 1.90f,  1.62f, 0f), // 1 오른쪽 위
-            new Vector3( 1.18f, -0.62f, 0f), // 2 오른쪽 아래
-            new Vector3(-1.18f, -0.62f, 0f), // 3 왼쪽 아래
-            new Vector3(-1.90f,  1.62f, 0f), // 4 왼쪽 위
+            new Vector3( 1.56f,  2.25f, 0f), // 1 오른쪽 위
+            new Vector3( 1.95f,  0.55f, 0f), // 2 오른쪽
+            new Vector3( 0.87f, -0.80f, 0f), // 3 오른쪽 아래
+            new Vector3(-0.87f, -0.80f, 0f), // 4 왼쪽 아래
+            new Vector3(-1.95f,  0.55f, 0f), // 5 왼쪽
+            new Vector3(-1.56f,  2.25f, 0f), // 6 왼쪽 위
         };
 
         int[] triangles = new int[]
@@ -19,6 +21,8 @@
             0, 1, 2,
             0, 2, 3,
             0, 3, 4,
+            0, 4, 5,
+            0, 5, 6,
         };
 
         Mesh mesh = new Mesh();
