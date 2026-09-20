e24d8ba8117b32866cab221d523f2ed56dc97104
Velde <cloudtail143@ewha.ac.kr>
2026-09-20 22:03:38 +0900

    S04: 다이아몬드 만들기

--- Assets/S04/S04_CustomDiamondMesh.cs
+++ Assets/S04/S04_CustomDiamondMesh.cs
@@ -24,6 +24,10 @@ public class S04_CustomDiamondMesh : MonoBehaviour
             3, 4, 0,
 
             // 아래쪽 4면 (정점 5 사용)
+            0, 1, 5,
+            1, 2, 5,
+            2, 3, 5,
+            3, 0, 5,
         };
 
         Mesh mesh = new Mesh();
